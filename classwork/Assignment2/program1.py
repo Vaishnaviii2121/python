976ddb9ffe17b6c0149e4bb997c77d4cc1e4ee49
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-01-21 18:29:55 +0530

    Implement multiplication program in program1.py

---
+++ classwork/Assignment2/program1.py
@@ -0,0 +1,15 @@
+def Multiplication(No):
+
+    for i in range(1,11): 
+        print(No * i) 
+
+def main():
+    value = int(input("Enter Number :"))
+    Ret = 0
+
+    Ret = Multiplication(value)
+
+    print(Ret)
+
+if __name__=="__main__":
+    main()
