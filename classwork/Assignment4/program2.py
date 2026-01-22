d7d85e37f7684af8054595fa68849bdd6d9f98f4
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-01-22 23:41:08 +0530

    Add basic arithmetic operations in program2.py

---
+++ classwork/Assignment4/program2.py
@@ -0,0 +1,11 @@
+def main():
+    num1 = int(input("Enter first Number :"))
+    num2 = int(input("Enter second Number :"))
+
+    print("Addition is :",num1+num2)
+    print("Subtraction is :",num1-num2)
+    print("Multiplication is :",num1*num2)
+    print("Division is :",num1/num2)
+
+if __name__=="__main__":
+    main()
