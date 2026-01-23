58e2d8468882f6698ad09a4d076738cf50609d02
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-01-23 11:19:13 +0530

    Add program1.py to calculate square of a number using Lambda function

---
+++ classwork/Assignment6/program1.py
@@ -0,0 +1,10 @@
+square = lambda no : (no * no)
+    
+def main():
+    value = int(input("Enter Number:"))
+    Ret = square(value)
+
+    print("square :",Ret)
+
+if __name__=="__main__":
+    main()
