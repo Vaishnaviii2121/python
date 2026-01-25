1287e8ffe524d789b3d7f49ad5e1ac0c12f19e26
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-01-26 00:12:06 +0530

    Create program2.py

---
+++ classwork/Assignment11/program2.py
@@ -0,0 +1,11 @@
+Multiplication = lambda No1,No2 : No1 * No2
+
+def main():
+    value1 = int(input())
+    value2 = int(input())
+
+    Ret = Multiplication(value1,value2)
+    print(Ret)
+
+if __name__ == "__main__":
+    main()
