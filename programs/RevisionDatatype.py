4483d9e85600eba118dfcfebbd64a293a37719bf
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-01-31 23:18:22 +0530

    Create RevisionDatatype.py

---
+++ programs/RevisionDatatype.py
@@ -0,0 +1,18 @@
+# Numeric Datatype
+
+print("-----------------------------------")       
+No = 11
+print(No)
+print(type(No))
+print("-----------------------------------")
+
+marks = 99.78
+print(marks)
+print(type(marks))
+print("-----------------------------------")
+
+x = 3+5j
+print(x)
+print(type(x))
+print("-----------------------------------")
+
