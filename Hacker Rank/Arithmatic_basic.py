80ba226ddc01091988a71292325941cfd2484eeb
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-04-12 09:16:59 +0530

    solved basic arithmetic problem

---
+++ Hacker Rank/Arithmatic_basic.py
@@ -0,0 +1,16 @@
+def Addition(No1,No2):
+    return No1 + No2
+
+def Subtraction(No1,No2):
+    return No1-No2
+
+def Multiplication(No1,No2):
+    return No1 * No2
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(Addition(a,b))
+    print(Subtraction(a,b))
+    print(Multiplication(a,b))
