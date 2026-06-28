df86929b9f1b3534316ea41b4191a22683a325c6
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-06-28 07:24:12 +0530

    Add nested function Zara in Phoenix
    Define nested function Zara inside Phoenix, but call fails.

---
+++ programs/FunctionDemo13.py
@@ -0,0 +1,11 @@
+def Phoenix():
+    print("Inside Phoenix Mall")
+
+    def Zara():
+        print("Inside ZARA")
+
+def main():
+   Phoenix.zara()       #Error
+    
+if __name__ =="__main__":
+    main()
