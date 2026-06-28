f6351e177a69ce1ad14a10024d2db4074ee7efca
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-06-28 07:23:21 +0530

    function call

---
+++ programs/FunctionDemo12.py
@@ -0,0 +1,11 @@
+def Phoenix():
+    print("Inside Phoenix Mall")
+
+    def Zara():
+        print("Inside ZARA")
+
+def main():
+   zara()       # Error
+    
+if __name__ =="__main__":
+    main()
