0c2cfba303a1869df2f0a587984403a2239f09d1
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-06-30 07:04:10 +0530

    Add function to check if a number is even or odd

---
+++ programs/SelectionEvenFunctionXX.py
@@ -0,0 +1,16 @@
+# procedural
+
+def ChechEven(no):
+    if(no % 2 == 0):
+        print("It is Even")
+    else:
+        print("It is Odd")
+
+
+Value = 0       
+
+print("Enter Number :")
+Value = int(input())
+
+ChechEven(Value)
+
