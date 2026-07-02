0cfcaed5b75f2131d9858ce9e6ee19785a78ea7d
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-07-02 07:18:05 +0530

    Add LocalGlobal1.py demonstrating variable scope

---
+++ programs/LocalGlobal1.py
@@ -0,0 +1,9 @@
+no = 11             # Global variable   ,no = 11 it is called as Data defination
+
+def fun():
+    no = 21         # Local Variable
+    print("Value of no from fun is : ",no)      # 21
+
+print("value of no is :",no)            # 11
+fun()
+
