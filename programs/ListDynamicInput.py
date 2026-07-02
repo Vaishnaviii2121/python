a6722400ecff854dc9d585418cfab208e90f0930
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-07-02 07:25:13 +0530

    Create ListDynamicInput.py for user input
    Implement a program to collect and display dynamic input.

---
+++ programs/ListDynamicInput.py
@@ -0,0 +1,17 @@
+def main():
+    size = 0
+
+    print("Enter the Number of Element :")
+    size = int(input())
+
+    Data = list()
+    
+    Data.append(10)
+    Data.append(20)
+    Data.append(30)
+    Data.append(40)
+
+    print(Data)
+    
+if __name__ == "__main__":
+    main()
