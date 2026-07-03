2a9498c3456568eeaf58830ca5324a595b8e4c7a
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-07-03 14:09:22 +0530

    Add dynamic list input functionality
    Implement a program to dynamically input a list of integers from the user.

---
+++ programs/ListDynamicInput1.py
@@ -0,0 +1,16 @@
+def main():
+    size = 0
+    value = 0
+
+    print("Enter the Number of Element :")
+    size = int(input())
+
+    Data = list()
+    
+    print("Enter the elements :")
+    for i in range(size):
+        value = int (input())
+        Data.append(value)
+    
+if __name__ == "__main__":
+    main()
