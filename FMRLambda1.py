1789bd1b10e991e44e9c483de33e5061a30c6961
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-07-04 06:49:01 +0530

    Implement lambda functions for data processing

---
+++ FMRLambda1.py
@@ -0,0 +1,34 @@
+from functools import reduce
+
+# def CheckEven(no):
+#  return (no % 2 ==0)
+
+CheckEven = lambda no : (no % 2 == 0)
+
+#def Increament(no):
+#   return no + 1
+
+Increament = lambda no : (no + 1)
+
+#def Add(A,B):
+#   return A + B
+
+Add = lambda A,B : (A + B)
+
+def main():
+    Data = [11,10,15,20,22,27,30]
+
+    print("Actual Data is :",Data)
+
+    FData = list(filter(CheckEven,Data))
+    print("Data after Filter is :",FData)
+
+    MData = list(map(Increament,FData))
+    print("Data after map is :",MData)
+
+    RData = reduce(Add,MData)
+    print("Data after Reduce is :",RData)
+
+if __name__ == "__main__":
+    main()
+
