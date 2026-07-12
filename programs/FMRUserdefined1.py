8d86d75a65309097a6c3a7446b27eae369991ae0
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-07-12 18:31:32 +0530

    Add filtering, mapping, and reducing functions
    This script defines functions for filtering, mapping, and reducing a list of numbers. It filters even numbers, increments them, and then sums the results.

---
+++ programs/FMRUserdefined1.py
@@ -0,0 +1,45 @@
+from functools import reduce
+
+CheckEven = lambda no : (no % 2 == 0)
+
+Increament = lambda no : (no + 1)
+
+Add = lambda A,B : (A + B)
+
+def filterX(task,elements):
+    Result = list()
+
+    for no in elements:
+        Ret = task(no)
+
+        if(Ret == True):
+            Result.append(no)
+
+    return Result
+
+def mapX(task,elements):
+    Result = list()
+
+    for no in elements:
+        Ret = task(no)
+        Result.append(Ret)
+
+    return Result
+
+def main():
+    Data = [11,10,15,20,22,27,30]
+
+    print("Actual Data is :",Data)
+
+    FData = list(filterX(CheckEven,Data))
+    print("Data after Filter is :",FData)
+
+    MData = list(mapX(Increament,FData))
+    print("Data after map is :",MData)
+
+    RData = reduce(Add,MData)
+    print("Data after Reduce is :",RData)
+
+if __name__ == "__main__":
+    main()
+
